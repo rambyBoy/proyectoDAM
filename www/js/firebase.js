faf8ff37d7c877ae4e5d9a9f1252f78804858bf8
Marcot A. G. Toriz <marcotoriz.d@gmail.com>
2018-11-13 19:37:40 -0600

    add back button event

---
+++ www/js/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+      apiKey: "AIzaSyCZqjylfQ2WSBP9uTfqx4VeW4jKuTXWwC8",
+      authDomain: "apt-helix-216517.firebaseapp.com",
+      databaseURL: "https://apt-helix-216517.firebaseio.com",
+      projectId: "apt-helix-216517",
+      storageBucket: "apt-helix-216517.appspot.com",
+      messagingSenderId: "1009348047012"
+    };
+    firebase.initializeApp(config);
